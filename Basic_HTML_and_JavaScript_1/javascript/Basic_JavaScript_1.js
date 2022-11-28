e62152144d8c50195db2a930acab5346a1c9e38f
Alex745i <alex745i@gmail.com>
2022-11-28 12:24:13 -0800

    correction

---
+++ Basic_HTML_and_JavaScript_1/javascript/Basic_JavaScript_1.js
@@ -0,0 +1,13 @@
+alert("Basic_JavaScript_1.js");
+    <button onclick="My_First_Function()">
+		Click me!
+	</button>
+	<p id="Irish"></p>
+<script>
+	function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+</script>
